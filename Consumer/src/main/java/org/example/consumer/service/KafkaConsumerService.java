ee56b591751d16a95d1c6b405102f5c4a0f23cd3
Ga0Lee <leegy0419@gmail.com>
2025-02-07 23:33:55 +0900

    feat: Consumer group1 파티션 수동 할당 제거, 컨테이너에서 파티션 자동 분배 처리
    - 수동으로 지정한 파티션 설정을 제거하고, 컨슈머 컨테이너가 파티션을 자동으로 할당받도록 변경했습니다.

--- Consumer/src/main/java/org/example/consumer/service/KafkaConsumerService.java
+++ Consumer/src/main/java/org/example/consumer/service/KafkaConsumerService.java
@@ -10,21 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 
-//    @KafkaListener(topicPartitions = @TopicPartition(topic = "${spring.kafka.consumer.topic1}", partitions = {"0"}), groupId = "${spring.kafka.consumer.group-id1}")
     @KafkaListener(topics = "${spring.kafka.consumer.topic1}", groupId = "${spring.kafka.consumer.group-id1}")
     public void consume1_1(ConsumerRecord<String, String> record) {
         log.info("[Consumer Group 1-1] Partition: {}, Offset: {}, Message: {}",
                 record.partition(), record.offset(), record.value());
     }
 
-//    @KafkaListener(topicPartitions = @TopicPartition(topic = "${spring.kafka.consumer.topic1}", partitions = {"1"}), groupId = "${spring.kafka.consumer.group-id1}")
     @KafkaListener(topics = "${spring.kafka.consumer.topic1}", groupId = "${spring.kafka.consumer.group-id1}")
     public void consume1_2(ConsumerRecord<String, String> record) {
         log.info("[Consumer Group 1-2] Partition: {}, Offset: {}, Message: {}",
                 record.partition(), record.offset(), record.value());
     }
 
-//    @KafkaListener(topicPartitions = @TopicPartition(topic = "${spring.kafka.consumer.topic1}", partitions = {"2"}), groupId = "${spring.kafka.consumer.group-id1}")
     @KafkaListener(topics = "${spring.kafka.consumer.topic1}", groupId = "${spring.kafka.consumer.group-id1}")
     public void consume1_3(ConsumerRecord<String, String> record) {
         log.info("[Consumer Group 1-3] Partition: {}, Offset: {}, Message: {}",
